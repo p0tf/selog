8c9817ef01128125acafb61706c40bea019b0949
watcol <kantaa058@gmail.com>
2020-09-19 22:44:28 +0900

    Done minimal implementation of Log trait.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! Simple, Easy LOGger logs to StdErr.
+extern crate log;
+
+use log::{Log, Metadata, Record};
+
+/// The SELog struct.
+pub struct SELog;
+
+impl Log for SELog {
+    fn enabled(&self, _metadata: &Metadata<'_>) -> bool {
+        true
     }
+
+    fn log(&self, record: &Record<'_>) {
+        eprintln!("{}: {}", record.level().to_string().to_lowercase(), record.args())
+    }
+
+    fn flush(&self) {}
 }
