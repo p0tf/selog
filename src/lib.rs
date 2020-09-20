7298ee1e14f11859063e7121f12118de1153b140
watcol <kantaa058@gmail.com>
2020-09-20 21:50:37 +0900

    Add methods to initialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 //! Simple, Easy LOGger logs to StdErr.
 extern crate log;
 
-use log::{Log, Metadata, Record};
+use log::{set_boxed_logger, set_max_level, LevelFilter, Log, Metadata, Record, SetLoggerError};
 
 /// The SELog struct.
+#[derive(Clone)]
 pub struct SELog;
 
 impl Log for SELog {
@@ -12,8 +13,25 @@ impl Log for SELog {
     }
 
     fn log(&self, record: &Record<'_>) {
-        eprintln!("{}: {}", record.level().to_string().to_lowercase(), record.args())
+        eprintln!(
+            "{}: {}",
+            record.level().to_string().to_lowercase(),
+            record.args()
+        )
     }
 
     fn flush(&self) {}
+}
+
+impl SELog {
+    /// Create new SELog.
+    pub fn new() -> Self {
+        SELog
+    }
+
+    /// Set logger as active.
+    pub fn init(&self) -> Result<(), SetLoggerError> {
+        set_max_level(LevelFilter::max());
+        set_boxed_logger(Box::new(self.clone()))
+    }
 }
