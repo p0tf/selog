b858948acb7229b93ada1b5f5bcf95651c4145a8
watcol <kantaa058@gmail.com>
2020-10-04 18:51:05 +0900

    Add trait "SEOpts".

--- src/opts.rs
+++ src/opts.rs
@@ -1,7 +1,10 @@
 //! The Integration with Clap(v3)
 extern crate clap;
 
-/// The macro to define struct which implements `clap::Clap` trait.
+use crate::{Colorize, SELevel, SELog};
+use log::SetLoggerError;
+
+/// A macro to define struct which implements `clap::Clap` trait.
 #[macro_export]
 macro_rules! opts {
     (
@@ -21,4 +24,32 @@ macro_rules! opts {
             $($(#[$subattr])* $fpub $field: $type,)*
         }
     };
+}
+
+/// A trait to initialize `SELog` with struct.
+pub trait SEOpts {
+    /// Get the value of `verbose`.
+    fn verbose(&self) -> bool;
+
+    /// Get the value of `quiet`.
+    fn quiet(&self) -> bool;
+
+    /// Get the value of `debug`.
+    fn debug(&self) -> bool;
+
+    /// Get the value of `no_output`.
+    fn color(&self) -> Colorize;
+
+    /// Initialize `SELog`.
+    fn init_log(&self) -> Result<(), SetLoggerError> {
+        SELog::new()
+            .level(
+                SELevel::new()
+                    .verbose(self.verbose())
+                    .quiet(self.quiet())
+                    .debug(self.debug()),
+            )
+            .colorize(self.color())
+            .init()
+    }
 }
